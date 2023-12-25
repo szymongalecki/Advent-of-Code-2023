81ae2a04f1f262154e1d87a0a0cd64edc735972d
Szymon Gałecki <szymongalecki@gmail.com>
2023-12-25 17:58:17 +0100

    Day 9 of AoC23

---
+++ Day9/2.py
@@ -0,0 +1,16 @@
+"""
+url:
+https://adventofcode.com/2023/day/9
+"""
+
+
+def sequence(s: list[int]) -> int:
+    if all(n == 0 for n in s):
+        return 0
+    return s[0] - sequence([b - a for (a, b) in zip(s[:-1], s[1:])])
+
+
+with open("input.txt") as f:
+    lines = [[int(n) for n in l.split()] for l in f.readlines()]
+    result = sum([sequence(s) for s in lines])
+    print(result)
